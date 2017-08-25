22cb68f117c89fba26e5cfc1f7cc00144261a106
Hottierceron <31248920+Hottierceron@users.noreply.github.com>
2017-08-25 15:13:37 -0400

    Add files via upload

--- LaunchMuledump.bat
+++ LaunchMuledump.bat
@@ -1,27 +1,26 @@
 @if (@CodeSection == @Batch) @then
 @echo off
 
-if exist muledump-2-master.exe (
-    rem file exists
-	echo found muledump-2-master.exe ... skipping Go build
+if exist muledump2.exe (
+	echo found muledump2.exe ... 
 ) else (
-    rem file doesn't exist
-	echo not found muledump-2-master.exe ... Running Go Build initial install
-	C:\Go\bin\go.exe build -o muledump-2-master.exe
+	echo did not find muledump2.exe ... 
+	pause
+	goto :EOF
 )
 
-echo updating accounts
+echo Transfering data from accounts.js to Muledump
 Regex.exe accounts.js
 
 setlocal
 set "getclip=cscript /nologo /e:JScript "%~f0""
 %getclip% > config.json
 
-
+echo Launching Webpage
 start "" http://localhost:5353/muledump.html
-echo Executing script
-muledump-2-master.exe "config.json"
-echo Error: Most likely cause, Error in "accounts.js" please check
+echo Launching Muledump.exe
+muledump2.exe "config.json"
+echo Error: Most likely cause, Error in "accounts.js" please check accounts.js
 pause
 goto :EOF
 
